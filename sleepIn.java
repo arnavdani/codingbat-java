97d519c930ae8c483ffdadda1942760c776703c4
arnavdani <60204322+arnavdani@users.noreply.github.com>
2020-01-28 20:52:39 -0800

    First checkin from codingbat

---
+++ sleepIn.java
@@ -0,0 +1,19 @@
+/* README : The parameter weekday is true if it is a weekday, 
+   and the parameter vacation is true if we are on vacation. 
+   We sleep in if it is not a weekday or we're on vacation. 
+   Return true if we sleep in.
+
+   sleepIn(false, false) → true
+   sleepIn(true, false) → false
+   sleepIn(false, true) → true
+
+   https://codingbat.com/prob/p187868
+
+*/
+
+public boolean sleepIn(boolean weekday, boolean vacation) {
+  if (!weekday || vacation){
+    return true;
+  }
+  return false;
+}
